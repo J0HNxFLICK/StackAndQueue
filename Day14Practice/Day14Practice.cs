b0e8e123e54405f20e6403eb01df4562f5b2f1c5
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-19 08:46:42 +0530

    [Arun] Created linked list stack

---
+++ Day14Practice/Day14Practice.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Linq;
+
+namespace Day14Practice
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+
+        }
+    }
+
+    class Stack
+    {
+        private Node top = null;
+
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (top == null)
+                node.next = null;
+            else
+                node.next = top;
+                top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+    internal class Day14Practice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack ,Queue , Linked List.");
+
+            Stack st = new Stack();
+
+            //UC1 Created stack of 56->30->70
+            st.Push(70);
+            st.Push(30);
+            st.Push(56);
+            st.Display();
+
+        }
+    }
+}
